16bc424ee2914a4070a66c70b05d65b2e1543eda
FelipePy <felipespsampaiomotasp@gmail.com>
2020-11-14 19:39:34 -0300

    Alteração da criação do banco de dados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 
 INSERT INTO users (`name`, `username`, `password`, `created_at`, `updated_at`, `role`, `status`) VALUES ('foo bar', 'foo', '$2y$12$fPqQBe.GDMBsuruFAYNApOJ3nIJ6k8WSI4urNcAK8lFJMuwNLNA1u', '2020-11-05 00:41:27.469193', '2020-11-05 00:41:27.469193', 'ADMIN', 'ACTIVE');
+INSERT INTO users (`name`, `username`, `password`, `created_at`, `updated_at`, `role`, `status`) VALUES ('LU', 'LU', '$2y$12$fPqQBe.GDMBsuruFAYNApOJ3nIJ6k8WSI4urNcAK8lFJMuwNLNA1u', '2020-11-05 00:41:27.469193', '2020-11-05 00:41:27.469193', 'MANAGER', 'ACTIVE');
+INSERT INTO users (`name`, `username`, `password`, `created_at`, `updated_at`, `role`, `status`) VALUES ('ZÉ', 'Ze', '$2y$12$fPqQBe.GDMBsuruFAYNApOJ3nIJ6k8WSI4urNcAK8lFJMuwNLNA1u', '2020-11-05 00:41:27.469193', '2020-11-05 00:41:27.469193', 'EMPLOYEE', 'ACTIVE');
